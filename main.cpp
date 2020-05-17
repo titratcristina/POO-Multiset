1be112e7fb235ac92ff07aa34eae1d880526317f
Titrat Cristina <titratcristina@gmail.com>
2020-05-17 18:53:40 +0300

    Check with assert elements in Multiset

--- main.cpp
+++ main.cpp
@@ -13,6 +13,11 @@ void multisetTests() {
 	M.insert(2);
 	M.insert(3);
 	M.insert(4);
+
+	assert(M.find(2) == 1);        	// verific dacă multisetul conține valoarea 2
+	assert(M.find(5) == 0);        	// verific dacă multisetul nu conține valoarea 5
+	assert(M.count(1) == 3);        // verific dacă numărul aparițiilor valorii 1 este 3
+
 }
 
 int main() {
