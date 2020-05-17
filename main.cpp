925aadb2b509f10a3314eafe8167dfb0f8f12aba
Titrat Cristina <titratcristina@gmail.com>
2020-05-17 18:55:39 +0300

    Remove all 1 elements from Multiset

--- main.cpp
+++ main.cpp
@@ -18,8 +18,11 @@ void multisetTests() {
 	assert(M.find(5) == 0);        	// verific dacă multisetul nu conține valoarea 5
 	assert(M.count(1) == 3);        // verific dacă numărul aparițiilor valorii 1 este 3
 
-	M.remove(1);                // se sterge ultima valoare din multiset a numărului 1
+	M.remove(1);            	// se sterge ultima valoare din multiset a numărului 1
 	assert(M.count(1) == 2);        // verific dacă numărul aparițiilor valorii 1 după ștergere este 2
+
+	M.removeAll(1);          	// se sterg toate aparitiile lui 1
+	assert(M.count(1) == 0);        // verific dacă numărul aparițiilor valorii 1 după ștergerea totală este 2
 
 }
 
