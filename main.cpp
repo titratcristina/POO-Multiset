da439edc80a127f00893615f699c674e5c69e14e
Titrat Cristina <titratcristina@gmail.com>
2020-05-17 18:53:08 +0300

    Add multisetTests function in main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <cassert>
+#include "Multiset.h"
+
 using namespace std;
 
+void multisetTests() {
+	Multiset<int> M, N;
+
+	M.insert(1);
+	M.insert(1);
+	M.insert(1);
+	M.insert(2);
+	M.insert(3);
+	M.insert(4);
+}
+
 int main() {
-	cout << "Hello, World!" << endl;
+	multisetTests();
+	cout << "Testele au trecut!" << endl;
 	return 0;
 }
