29e11aedc2316a40af35ead4b8bc70b1f26e3235
Titrat Cristina <titratcristina@gmail.com>
2020-05-17 18:24:34 +0300

    Add Comparator class

---
+++ Comparator.h
@@ -0,0 +1,14 @@
+//
+// Created by Cristina Titrat on 17/05/2020.
+//
+
+#ifndef POO_MULTISET_COMPARATOR_H
+#define POO_MULTISET_COMPARATOR_H
+
+template<class T>
+class Comparator {
+public:
+	static bool equal(T, T);
+};
+
+#endif //POO_MULTISET_COMPARATOR_H
