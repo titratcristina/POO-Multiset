24ceb608c1a9084fc0958cfa711691e862eaec39
Titrat Cristina <titratcristina@gmail.com>
2020-05-17 18:54:04 +0300

    Remove an element from Multiset

--- main.cpp
+++ main.cpp
@@ -18,6 +18,9 @@ void multisetTests() {
 	assert(M.find(5) == 0);        	// verific dacă multisetul nu conține valoarea 5
 	assert(M.count(1) == 3);        // verific dacă numărul aparițiilor valorii 1 este 3
 
+	M.remove(1);                // se sterge ultima valoare din multiset a numărului 1
+	assert(M.count(1) == 2);        // verific dacă numărul aparițiilor valorii 1 după ștergere este 2
+
 }
 
 int main() {
